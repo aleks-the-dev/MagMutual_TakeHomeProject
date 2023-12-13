e38fa7a52a0826f86d26e28451e093ba38de6fe9
Aleks <aleks@Alekss-MBP.attlocal.net>
2023-12-13 17:13:16 -0500

    Added some comments and ideas for UI improvement.

--- src/main/java/com/takehomeproject/MagMutual_AleksBelotserkovskaya/WebController.java
+++ src/main/java/com/takehomeproject/MagMutual_AleksBelotserkovskaya/WebController.java
@@ -17,6 +17,7 @@ public class WebController {
 		return "greeting";
 	}
 	
+	// Endpoint to return a specific user (and all associated information)
 	@GetMapping("/specificUser")
 	public String specificUser(@RequestParam(value="id", required=false, defaultValue="World") String id, Model model) {
 		model.addAttribute("id", id);
@@ -26,6 +27,7 @@ public class WebController {
 		return "specificuser";
 	}
 	
+	// Endpoint to return a list of users created between a date range
 	@GetMapping("/usersByDateRange") // TODO 
 	public String usersByDateRange(@RequestParam(value="date1", required=false, defaultValue="World") String date1, 
 			@RequestParam(value="date2", required=false, defaultValue="World") String date2, Model model) {
@@ -38,6 +40,7 @@ public class WebController {
 		return "usersbydaterange";
 	}
 	
+	// Endpoint to return a list of users based on a specific profession
 	@GetMapping("/usersByJob") // TODO fix
 	public String usersByProfession(@RequestParam(value="job", required=false, defaultValue="World") String job, Model model) {
 		model.addAttribute("job", job);
@@ -48,6 +51,7 @@ public class WebController {
 		return "usersbyjob";
 	}
 	
+	// Custom Endpoint that I designed on my own (get list of users by country).
 	@GetMapping("/usersByCountry") // TODO
 	public String customEndpoint(@RequestParam(name="country", required=false, defaultValue="World") String country, Model model) {
 		model.addAttribute("country", country);
@@ -57,10 +61,22 @@ public class WebController {
 		model.addAttribute("usersByCountry", usersByCountry);
 		return "usersbycountry";
 	}
+	
+	// ideas for verification and error messages: 
+	// incorrect city/country combo
+	// how to allow for user to input csv file + check for incorrect columns or data or format
+	// no user found, wrong spelling of name, etc. 
+	
+	// ideas for improving ui: 
+	// add buttons to screen for ease of usability 
+	// add text boxes to select user id
+	// or could choose dropdown of user id's
+	
+	// could do dropdown of countries
+	// date range - how to enter? 
+	// could also do dropdown of professions
+	
+	// maybe when a list appears, a user could click button next to each listed user to see their full info 
+	// (might be too complex to do by project deadline)
 
 }
-
-// - Endpoint to return a specific user (and all associated information)
-// - Endpoint to return a list of users created between a date range
-// - Endpoint to return a list of users based on a specific profession
-// - Custom Endpoint that you design on your own.
